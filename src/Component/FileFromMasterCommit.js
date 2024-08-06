8aa7c2f78585f9c6a62899409b489c24c2ed5863
vimal <vimal.murugan@securekloud.com>
2024-08-06 23:23:55 +0530

    in test 2

--- src/Component/FileFromMasterCommit.js
+++ src/Component/FileFromMasterCommit.js
@@ -2,6 +2,10 @@ const Commit=()=>{
     return(
         <div>
             1) first line in test 2 brach
+
+            changes in test 1 
+
+            lorem ipusum something
         </div>
     )
 }
