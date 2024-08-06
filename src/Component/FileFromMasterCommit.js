665dffd8f116e114626227c3816c44ed6239533a
vimal <vimal.murugan@securekloud.com>
2024-08-06 23:23:03 +0530

    master change

--- src/Component/FileFromMasterCommit.js
+++ src/Component/FileFromMasterCommit.js
@@ -2,6 +2,10 @@ const Commit=()=>{
     return(
         <div>
             1) first line in test 2 brach
+
+
+            2) chages from master 1
+            3) chanf3es sosdods master
         </div>
     )
 }
