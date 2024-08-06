0169300e7bec51159d3be23019822f9cf7af65fd
vimal <vimal.murugan@securekloud.com>
2024-08-06 23:41:35 +0530

    Revert "Merge remote-tracking branch 'origin/test-2'"
    This reverts commit 678f1e1a50ce3afd45b2fe3e26ba0e8df86bee7e, reversing
    changes made to 583fccca0ab0b0936b9b6658d8fa5ddcb6a9372b.

--- src/Component/FileFromMasterCommit.js
+++ src/Component/FileFromMasterCommit.js
@@ -1,14 +1,12 @@
-const Commit = () => {
-
-    const confilectCheck = () => {
-
-    }
-
-    return (
+const Commit=()=>{
+    return(
         <div>
             1) first line in test 2 brach
 
 
+            2) chages from master 1
+            3) chanf3es sosdods master
+            changes in test 1 
 
             lorem ipusum something
         </div>
